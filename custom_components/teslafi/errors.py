976537f7e75ba72991937a19a7ce6f75542a3b08
Joe Hansche <madcoder@gmail.com>
2024-04-13 11:41:32 -0400

    Raise API errors

--- custom_components/teslafi/errors.py
+++ custom_components/teslafi/errors.py
@@ -1,2 +1,9 @@
+from dataclasses import dataclass
+
+
 class VehicleNotReadyError(Exception):
     """The vehicle is sleeping"""
+
+
+class TeslaFiApiError(Exception):
+    """API responded with an error reason"""
