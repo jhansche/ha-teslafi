4ca3d8baeda848a72a580a072ae61631f3d4bd45
James Sheputis <jsheputis@users.noreply.github.com>
2025-03-27 18:52:32 -0500

    Finish test cases for model class

--- tests/test_model.py
+++ tests/test_model.py
@@ -294,6 +294,15 @@ def test_TeslaFiVehicle_shift_state_not_driving():
     
     assert vehicle.shift_state == "park"
 
+def test_TeslaFiVehicle_shift_state_None():
+    """Test TeslaFiVehicle class shift_state property"""
+    vehicle_data = {
+    }
+    
+    vehicle = TeslaFiVehicle(vehicle_data)
+    
+    assert vehicle.shift_state is None
+
 def test_TeslaFiVehicle_is_locked():
     """Test TeslaFiVehicle class is_locked property"""
     vehicle_data = {
@@ -332,3 +341,274 @@ def test_TeslaFiVehicle_charging_state_none():
     vehicle = TeslaFiVehicle(vehicle_data)
     
     assert vehicle.charging_state is None
+    
+def test_TeslaFiVehicle_charge_session_number():
+    """Test TeslaFiVehicle class charge_session_number property"""
+    vehicle_data = {
+        "chargeNumber": 123
+    }
+    
+    vehicle = TeslaFiVehicle(vehicle_data)
+    
+    assert vehicle.charge_session_number == 123
+
+def test_TeslaFiVehicle_charge_session_number_zero():
+    """Test TeslaFiVehicle class charge_session_number property"""
+    vehicle_data = {
+        "chargeNumber": 0
+    }
+    
+    vehicle = TeslaFiVehicle(vehicle_data)
+    
+    assert vehicle.charge_session_number is None
+    
+def test_TeslaFiVehicle_is_plugged_in():
+    """Test TeslaFiVehicle class is_plugged_in property"""
+    vehicle_data = {
+        "charging_state": "Charging"
+    }
+    
+    vehicle = TeslaFiVehicle(vehicle_data)
+    
+    assert vehicle.is_plugged_in is True
+
+def test_TeslaFiVehicle_is_plugged_in_False():
+    """Test TeslaFiVehicle class is_plugged_in property"""
+    vehicle_data = {
+        "charging_state": "Disconnected"
+    }
+    
+    vehicle = TeslaFiVehicle(vehicle_data)
+    
+    assert vehicle.is_plugged_in is False
+    
+def test_TeslaFiVehicle_is_charging():
+    """Test TeslaFiVehicle class is_charging property"""
+    vehicle_data = {
+        "charging_state": "Charging"
+    }
+    
+    vehicle = TeslaFiVehicle(vehicle_data)
+    
+    assert vehicle.is_charging is True
+
+def test_TeslaFiVehicle_is_charging_False():
+    """Test TeslaFiVehicle class is_charging property"""
+    vehicle_data = {
+        "charging_state": "Stopped"
+    }
+    
+    vehicle = TeslaFiVehicle(vehicle_data)
+    
+    assert vehicle.is_charging is False
+    
+def test_TeslaFiVehicle_is_fast_charger():
+    """Test TeslaFiVehicle class is_fast_charger property"""
+    vehicle_data = {
+        "charging_state": "Charging",
+        "fast_charger_present": True
+    }
+    
+    vehicle = TeslaFiVehicle(vehicle_data)
+    
+    assert vehicle.is_fast_charger is True
+
+def test_TeslaFiVehicle_is_fast_charger_default_False():
+    """Test TeslaFiVehicle class is_fast_charger property"""
+    vehicle_data = {
+        "charging_state": "Charging",
+    }
+    
+    vehicle = TeslaFiVehicle(vehicle_data)
+    
+    assert vehicle.is_fast_charger is False
+    
+def test_TeslaFiVehicle_charger_current():
+    """Test TeslaFiVehicle class charger_current property"""
+    vehicle_data = {
+        "charger_actual_current": 32
+    }
+    
+    vehicle = TeslaFiVehicle(vehicle_data)
+    
+    assert vehicle.charger_current == 32
+
+def test_TeslaFiVehicle_charger_current_none():
+    """Test TeslaFiVehicle class charger_current property"""
+    vehicle_data = {
+    }
+    
+    vehicle = TeslaFiVehicle(vehicle_data)
+    
+    assert vehicle.charger_current is None
+
+def test_TeslaFiVehicle_charger_current_fast():
+    """Test TeslaFiVehicle class charger_current property"""
+    vehicle_data = {
+        "charging_state": "Charging",
+        "fast_charger_present": True,
+        "charger_power": 200,
+        "charger_voltage": 100,
+        
+    }
+    
+    vehicle = TeslaFiVehicle(vehicle_data)
+    
+    assert vehicle.charger_current == 2
+
+def test_TeslaFiVehicle_charger_voltage():
+    """Test TeslaFiVehicle class charger_voltage property"""
+    vehicle_data = {
+        "charger_voltage": 240
+    }
+    
+    vehicle = TeslaFiVehicle(vehicle_data)
+    
+    assert vehicle.charger_voltage == 240
+    
+def test_TeslaFiVehicle_charger_level_1():
+    """Test TeslaFiVehicle class charger_level property"""
+    vehicle_data = {
+        "charging_state": "Charging",
+        "charger_voltage": 120
+    }
+    
+    vehicle = TeslaFiVehicle(vehicle_data)
+    
+    assert vehicle.charger_level == "level-1"
+
+def test_TeslaFiVehicle_charger_level_2():
+    """Test TeslaFiVehicle class charger_level property"""
+    vehicle_data = {
+        "charging_state": "Charging",
+        "charger_voltage": 240
+    }
+    
+    vehicle = TeslaFiVehicle(vehicle_data)
+    
+    assert vehicle.charger_level == "level-2"
+
+def test_TeslaFiVehicle_charger_level_3():
+    """Test TeslaFiVehicle class charger_level property"""
+    vehicle_data = {
+        "charging_state": "Charging",
+        "charger_voltage": 240,
+        "fast_charger_present": True
+    }
+    
+    vehicle = TeslaFiVehicle(vehicle_data)
+    
+    assert vehicle.charger_level == "level-3"
+
+def test_TeslaFiVehicle_charger_level_none():
+    """Test TeslaFiVehicle class charger_level property"""
+    vehicle_data = {
+        "charging_state": "Charging",
+    }
+    
+    vehicle = TeslaFiVehicle(vehicle_data)
+    
+    assert vehicle.charger_level is None
+
+def test_TeslaFiVehicle_charger_level_none_unplugged():
+    """Test TeslaFiVehicle class charger_level property"""
+    vehicle_data = {
+    }
+    
+    vehicle = TeslaFiVehicle(vehicle_data)
+    
+    assert vehicle.charger_level is None
+    
+def test_TeslaFiVehicle_is_defrosting_front():
+    """Test TeslaFiVehicle class is_defrosting property"""
+    vehicle_data = {
+        "is_front_defroster_on": "1"
+    }
+    
+    vehicle = TeslaFiVehicle(vehicle_data)
+    
+    assert vehicle.is_defrosting is True
+
+def test_TeslaFiVehicle_is_defrosting_rear():
+    """Test TeslaFiVehicle class is_defrosting property"""
+    vehicle_data = {
+        "is_front_defroster_on": "1"
+    }
+    
+    vehicle = TeslaFiVehicle(vehicle_data)
+    
+    assert vehicle.is_defrosting is True
+
+def test_TeslaFiVehicle_is_defrosting_mode():
+    """Test TeslaFiVehicle class is_defrosting property"""
+    vehicle_data = {
+        "defrost_mode": "1"
+    }
+    
+    vehicle = TeslaFiVehicle(vehicle_data)
+    
+    assert vehicle.is_defrosting is True
+
+def test_TeslaFiVehicle_is_defrosting_mode_False():
+    """Test TeslaFiVehicle class is_defrosting property"""
+    vehicle_data = {
+    }
+    
+    vehicle = TeslaFiVehicle(vehicle_data)
+    
+    assert vehicle.is_defrosting is False
+
+def test_TeslaFiVehicle_tpms():
+    """Test TeslaFiVehicle class tpms property"""
+    vehicle_data = {
+        "tpms_front_left": "43.0",
+        "tpms_front_right": "42.5",
+        "tpms_rear_left": "41.5",
+        "tpms_rear_right": "44.0",
+        "pressure": "psi",
+    }
+    
+    vehicle = TeslaFiVehicle(vehicle_data)
+    
+    assert isinstance(vehicle.tpms, TeslaFiTirePressure)
+    assert vehicle.tpms.front_left == 43.0
+    assert vehicle.tpms.front_right == 42.5
+    assert vehicle.tpms.rear_left == 41.5
+    assert vehicle.tpms.rear_right == 44.0
+    assert vehicle.tpms.unit == "psi"
+
+def test_TeslaFiVehicle_tpms_none():
+    """Test TeslaFiVehicle class tpms property"""
+    vehicle_data = {
+        "tpms_front_left": None,
+        "tpms_front_right": None,
+        "tpms_rear_left": None,
+        "tpms_rear_right": None
+    }
+    
+    vehicle = TeslaFiVehicle(vehicle_data)
+    
+    assert isinstance(vehicle.tpms, TeslaFiTirePressure)
+    assert vehicle.tpms.front_left is None
+    assert vehicle.tpms.front_right is None
+    assert vehicle.tpms.rear_left is None
+    assert vehicle.tpms.rear_right is None
+    assert vehicle.tpms.unit == "psi"
+
+def test_TeslaFiTirePressure_constructor():
+    """Test TeslaFiTirePressure class"""
+    
+    tire_pressure = TeslaFiTirePressure(
+        front_left=43.0,
+        front_right=42.5,
+        rear_left=41.5,
+        rear_right=44.0,
+        unit="psi",
+    )
+    
+    assert isinstance(tire_pressure, TeslaFiTirePressure)
+    
+def test_TeslaFiTirePressure_convert_unit():
+    """Test TeslaFiTirePressure class convert_unit method"""
+    
+    assert TeslaFiTirePressure.convert_unit("psi") == "psi"
